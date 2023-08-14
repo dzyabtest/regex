773f69da0f5af8a972a09d4d073ddaca7b709ef6
dzyabtest <dzyab@mail.ru>
2023-08-14 17:57:41 +0300

    Вариант 1

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,45 @@
+/* eslint-disable indent */
+/* eslint-disable linebreak-style */
+import Validator from '../app';
+
+test('validate user name 1', () => {
+  const result = Validator.validateUsername('user');
+
+  expect(result).toBe(true);
+});
+
+test('validate user name 2', () => {
+  const result = Validator.validateUsername('user123user');
+
+  expect(result).toBe(true);
+});
+
+test('validate user name 3', () => {
+    const result = Validator.validateUsername('user123user');
+
+    expect(result).toBe(true);
+  });
+
+  test('validate user name 4', () => {
+    const result = Validator.validateUsername('user123u-s_er');
+
+    expect(result).toBe(true);
+  });
+
+  test('validate user name 5', () => {
+    const result = Validator.validateUsername('1user123user');
+
+    expect(result).toBe(false);
+  });
+
+  test('validate user name 6', () => {
+    const result = Validator.validateUsername('user123user-');
+
+    expect(result).toBe(false);
+  });
+
+  test('validate user name 7', () => {
+    const result = Validator.validateUsername('user1234user');
+
+    expect(result).toBe(false);
+  });
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,13 @@
+/* eslint-disable indent */
 // TODO: write your code here
 import sum from './basic';
 
 console.log('worked');
 
 console.log(sum([1, 2]));
+
+export default class Validator {
+    static validateUsername(userName) {
+        return /^[a-zA-Z0-9_-]+$/.test(userName) && /^[^0-9_-]/.test(userName) && /[^0-9_-]$/.test(userName) && !/[0-9]{4,}/.test(userName);
+    }
+}
